dbbea7b28a8477c6a0bd7be8a7353dce79a44693
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-04-03 20:40:15 +0300

    Move style.css and html css into React

--- football/frontend/src/App.js
+++ football/frontend/src/App.js
@@ -3,21 +3,116 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header className="header">
+        <div className="brand">
+          <a href="index.html">Football-fanshop.com</a>
+        </div>
+        <div className="header-links">
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign In</a>
+        </div>
       </header>
+      <main className="main">
+        <div className="content">
+          <ul className="products">
+            <li>
+              <div className="product">
+                <img className="product-image"
+                  src="https://cdn.shopify.com/s/files/1/1055/5166/products/ADIDAMANUNITEDJERSEYH31447SHOPIFY_3_2400x.png?v=1628489228"
+                  alt="product" />
+                <div className="product-name">
+                  <a href="product.html">Manchester United men home shirt 2021/2022</a>
+                </div>
+                <div className="product-brand">Adidas</div>
+                <div className="product-price">79.99 лв.</div>
+                <div className="product-rating">5 Stars (10 Reviews)</div>
+              </div>
+            </li>
+            <li>
+              <div className="product">
+                <img className="product-image"
+                  src="https://cdn.shopify.com/s/files/1/1055/5166/products/ADIDAMANUNITEDJERSEYH31447SHOPIFY_3_2400x.png?v=1628489228"
+                  alt="product" />
+                <div className="product-name">
+                  <a href="product.html">Manchester United men home shirt 2021/2022</a>
+                </div>
+                <div className="product-brand">Adidas</div>
+                <div className="product-price">79.99 лв.</div>
+                <div className="product-rating">5 Stars (10 Reviews)</div>
+              </div>
+            </li>
+            <li>
+              <div className="product">
+                <img className="product-image"
+                  src="https://cdn.shopify.com/s/files/1/1055/5166/products/ADIDAMANUNITEDJERSEYH31447SHOPIFY_3_2400x.png?v=1628489228"
+                  alt="product" />
+                <div className="product-name">
+                  <a href="product.html">Manchester United men home shirt 2021/2022</a>
+                </div>
+                <div className="product-brand">Adidas</div>
+                <div className="product-price">79.99 лв.</div>
+                <div className="product-rating">5 Stars (10 Reviews)</div>
+              </div>
+            </li>
+            <li>
+              <div className="product">
+                <img className="product-image"
+                  src="https://cdn.shopify.com/s/files/1/1055/5166/products/ADIDAMANUNITEDJERSEYH31447SHOPIFY_3_2400x.png?v=1628489228"
+                  alt="product" />
+                <div className="product-name">
+                  <a href="product.html">Manchester United men home shirt 2021/2022</a>
+                </div>
+                <div className="product-brand">Adidas</div>
+                <div className="product-price">79.99 лв.</div>
+                <div className="product-rating">5 Stars (10 Reviews)</div>
+              </div>
+            </li>
+            <li>
+              <div className="product">
+                <img className="product-image"
+                  src="https://cdn.shopify.com/s/files/1/1055/5166/products/ADIDAMANUNITEDJERSEYH31447SHOPIFY_3_2400x.png?v=1628489228"
+                  alt="product" />
+                <div className="product-name">
+                  <a href="product.html">Manchester United men home shirt 2021/2022</a>
+                </div>
+                <div className="product-brand">Adidas</div>
+                <div className="product-price">79.99 лв.</div>
+                <div className="product-rating">5 Stars (10 Reviews)</div>
+              </div>
+            </li>
+            <li>
+              <div className="product">
+                <img className="product-image"
+                  src="https://cdn.shopify.com/s/files/1/1055/5166/products/ADIDAMANUNITEDJERSEYH31447SHOPIFY_3_2400x.png?v=1628489228"
+                  alt="product" />
+                <div className="product-name">
+                  <a href="product.html">Manchester United men home shirt 2021/2022</a>
+                </div>
+                <div className="product-brand">Adidas</div>
+                <div className="product-price">79.99 лв.</div>
+                <div className="product-rating">5 Stars (10 Reviews)</div>
+              </div>
+            </li>
+            <li>
+              <div className="product">
+                <img className="product-image"
+                  src="https://cdn.shopify.com/s/files/1/1055/5166/products/ADIDAMANUNITEDJERSEYH31447SHOPIFY_3_2400x.png?v=1628489228"
+                  alt="product" />
+                <div className="product-name">
+                  <a href="product.html">Manchester United men home shirt 2021/2022</a>
+                </div>
+                <div className="product-brand">Adidas</div>
+                <div className="product-price">79.99 лв.</div>
+                <div className="product-rating">5 Stars (10 Reviews)</div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </main>
+      <footer className="footer">
+        Tsvetomir Dimitrov © All rights reserved.
+      </footer>
     </div>
   );
 }
