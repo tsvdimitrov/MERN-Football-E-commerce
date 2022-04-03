b38242870b30c9fb6fe52b34f4cbc97dfb13610c
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-04-03 22:07:36 +0300

    Create data from JSON-server

--- football/frontend/src/App.js
+++ football/frontend/src/App.js
@@ -1,5 +1,9 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
+
 
 function App() {
 
@@ -9,136 +13,65 @@ function App() {
 
   const closeMenu = () => {
     document.querySelector('sidebar').classList.remove("open");
-
   }
-
   return (
-    <div className="grid-container">
-      <header className="header">
-        <div className="brand">
-          <button onClick={openMenu}>
-            &#9776;
-          </button>
-          <a href="index.html">Football-fanshop.com</a>
-        </div>
-        <div className="header-links">
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
-        </div>
-      </header>
-      <aside className="sidebar" >
-        <h3>Shopping Categories</h3>
-        <button className="sidebar-close-button" onClick={closeMenu}>x</button>
-        <ul>
-          <li>
-            <a href="index.html">Pants</a>
-          </li>
-          <li>
-            <a href="index.html">Shirts</a>
-          </li>
-        </ul>
-      </aside>
-      <main className="main">
-        <div className="content">
-          <ul className="products">
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>
+              &#9776;
+            </button>
+            <a href="index.html">Football-fanshop.com</a>
+          </div>
+          <div className="header-links">
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign In</a>
+          </div>
+        </header>
+        <aside className="sidebar" >
+          <h3>Shopping Categories</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+          <ul>
             <li>
-              <div className="product">
-                <img className="product-image"
-                  src="https://cdn.shopify.com/s/files/1/1055/5166/products/ADIDAMANUNITEDJERSEYH31447SHOPIFY_3_2400x.png?v=1628489228"
-                  alt="product" />
-                <div className="product-name">
-                  <a href="product.html">Manchester United men home shirt 2021/2022</a>
-                </div>
-                <div className="product-brand">Adidas</div>
-                <div className="product-price">79.99 лв.</div>
-                <div className="product-rating">5 Stars (10 Reviews)</div>
-              </div>
+              <a href="index.html">Pants</a>
             </li>
             <li>
-              <div className="product">
-                <img className="product-image"
-                  src="https://cdn.shopify.com/s/files/1/1055/5166/products/ADIDAMANUNITEDJERSEYH31447SHOPIFY_3_2400x.png?v=1628489228"
-                  alt="product" />
-                <div className="product-name">
-                  <a href="product.html">Manchester United men home shirt 2021/2022</a>
-                </div>
-                <div className="product-brand">Adidas</div>
-                <div className="product-price">79.99 лв.</div>
-                <div className="product-rating">5 Stars (10 Reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img className="product-image"
-                  src="https://cdn.shopify.com/s/files/1/1055/5166/products/ADIDAMANUNITEDJERSEYH31447SHOPIFY_3_2400x.png?v=1628489228"
-                  alt="product" />
-                <div className="product-name">
-                  <a href="product.html">Manchester United men home shirt 2021/2022</a>
-                </div>
-                <div className="product-brand">Adidas</div>
-                <div className="product-price">79.99 лв.</div>
-                <div className="product-rating">5 Stars (10 Reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img className="product-image"
-                  src="https://cdn.shopify.com/s/files/1/1055/5166/products/ADIDAMANUNITEDJERSEYH31447SHOPIFY_3_2400x.png?v=1628489228"
-                  alt="product" />
-                <div className="product-name">
-                  <a href="product.html">Manchester United men home shirt 2021/2022</a>
-                </div>
-                <div className="product-brand">Adidas</div>
-                <div className="product-price">79.99 лв.</div>
-                <div className="product-rating">5 Stars (10 Reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img className="product-image"
-                  src="https://cdn.shopify.com/s/files/1/1055/5166/products/ADIDAMANUNITEDJERSEYH31447SHOPIFY_3_2400x.png?v=1628489228"
-                  alt="product" />
-                <div className="product-name">
-                  <a href="product.html">Manchester United men home shirt 2021/2022</a>
-                </div>
-                <div className="product-brand">Adidas</div>
-                <div className="product-price">79.99 лв.</div>
-                <div className="product-rating">5 Stars (10 Reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img className="product-image"
-                  src="https://cdn.shopify.com/s/files/1/1055/5166/products/ADIDAMANUNITEDJERSEYH31447SHOPIFY_3_2400x.png?v=1628489228"
-                  alt="product" />
-                <div className="product-name">
-                  <a href="product.html">Manchester United men home shirt 2021/2022</a>
-                </div>
-                <div className="product-brand">Adidas</div>
-                <div className="product-price">79.99 лв.</div>
-                <div className="product-rating">5 Stars (10 Reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img className="product-image"
-                  src="https://cdn.shopify.com/s/files/1/1055/5166/products/ADIDAMANUNITEDJERSEYH31447SHOPIFY_3_2400x.png?v=1628489228"
-                  alt="product" />
-                <div className="product-name">
-                  <a href="product.html">Manchester United men home shirt 2021/2022</a>
-                </div>
-                <div className="product-brand">Adidas</div>
-                <div className="product-price">79.99 лв.</div>
-                <div className="product-rating">5 Stars (10 Reviews)</div>
-              </div>
+              <a href="index.html">Shirts</a>
             </li>
           </ul>
-        </div>
-      </main>
-      <footer className="footer">
-        Tsvetomir Dimitrov © All rights reserved.
-      </footer>
-    </div>
+        </aside>
+        <main className="main">
+          <div className="content">
+            <Route path="/products/:id" component={ProductScreen} />
+            <Route path="/" exact={true} component={HomeScreen} />
+            <ul className="products">
+              {
+                data.products.map(product =>
+                  <li>
+                    <div className="product">
+                      <img className="product-image"
+                        src="https://cdn.shopify.com/s/files/1/1055/5166/products/ADIDAMANUNITEDJERSEYH31447SHOPIFY_3_2400x.png?v=1628489228"
+                        alt="product" />
+                      <div className="product-name">
+                        <a href="product.html">{product.name}</a>
+                      </div>
+                      <div className="product-brand">{product.brand}</div>
+                      <div className="product-price">{product.price}</div>
+                      <div className="product-rating">{product.rating} Stars ({product.numReviews} Reviews)</div>
+                    </div>
+                  </li>)
+              }
+
+
+            </ul>
+          </div>
+        </main>
+        <footer className="footer">
+          Tsvetomir Dimitrov © All rights reserved.
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
