6943131bf212834b819facf814244ca6175283fd
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-04-03 21:34:03 +0300

    Create siderbar menu

--- football/frontend/src/App.js
+++ football/frontend/src/App.js
@@ -2,10 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open");
+  }
+
+  const closeMenu = () => {
+    document.querySelector('sidebar').classList.remove("open");
+
+  }
+
   return (
     <div className="grid-container">
       <header className="header">
         <div className="brand">
+          <button onClick={openMenu}>
+            &#9776;
+          </button>
           <a href="index.html">Football-fanshop.com</a>
         </div>
         <div className="header-links">
@@ -13,6 +26,18 @@ function App() {
           <a href="signin.html">Sign In</a>
         </div>
       </header>
+      <aside className="sidebar" >
+        <h3>Shopping Categories</h3>
+        <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+        <ul>
+          <li>
+            <a href="index.html">Pants</a>
+          </li>
+          <li>
+            <a href="index.html">Shirts</a>
+          </li>
+        </ul>
+      </aside>
       <main className="main">
         <div className="content">
           <ul className="products">
