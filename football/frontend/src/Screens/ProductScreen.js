8523f23f8eceed26792f6d1adbcebfac9feb6844
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-04-04 23:39:02 +0300

    Adding product details - image, name, rating, price and description

--- football/frontend/src/Screens/ProductScreen.js
+++ football/frontend/src/Screens/ProductScreen.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import data from '../data';
 
 function ProductScreen(props) {
     console.log(props.match.params.id)
     const product = data.product.find(x => x._id === props.match.params.id);
     return <div>
-        <h1>{product.name}</h1>
+        <div>
+            <Link to="/">Back to result</Link>
+        </div>
+        <div className="details"></div>
+        <div className="details-image">
+            <img src={product.image} alt="product"></img>
+        </div>
+        <div className="details-info" >
+            <ul>
+                <li>
+                    <h4>{product.name}</h4>
+                </li>
+                <li>
+                    {product.rating} Stars ({product.numReviews} Reviews)
+                </li>
+                <li>
+                    <b>{product.price}</b>
+                </li>
+                <li>
+                    Description:
+                    <div>
+                        {product.description}
+                    </div>
+                </li>
+            </ul>
+        </div>
     </div>
 }
 
