3a606ce18b19af27ed9c155a728e4b7c7c69ea43
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-04-09 10:41:17 +0300

    Create cart screen

--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 import { useContext, useEffect, useReducer } from 'react';
 import Row from 'react-bootstrap/Row';
@@ -29,6 +29,7 @@ const reducer = (state, action) => {
 };
 
 function ProductScreen() {
+    const navigate = useNavigate();
     const params = useParams();
     const { slug } = params;
     const [{ loading, error, product }, dispatch] = useReducer(reducer, {
@@ -63,6 +64,7 @@ function ProductScreen() {
             type: 'CART_ADD_ITEM',
             payload: { ...product, quantity },
         });
+        navigate('/cart');
     };
 
     return loading ? (
