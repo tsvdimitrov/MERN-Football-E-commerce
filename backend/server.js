54659b892e971b3b45b5330f88847ffaeda431ef
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-04-09 20:56:39 +0300

    Refactoring server due to errors

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,11 @@
 import express from 'express';
 import data from './data.js';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log('connected to db');
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
 
 const app = express();
 // test
 app.get('/api/products', (req, res) => {
   res.send(data.products);
-});
+})
 app.get('/api/products/slug/:slug', (req, res) => {
   const product = data.products.find((x) => x.slug === req.params.slug);
   if (product) {
@@ -35,8 +22,7 @@ app.get('/api/products/:id', (req, res) => {
     res.status(404).send({ message: 'Product Not Found' });
   }
 });
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
-});
+})
